a71b6399ef791aea151decdcf27cc647e258128d
Campos ADS <campos.ads@outlook.com>
2026-04-22 23:11:22 -0400

    atualizar photo

--- src/App.tsx
+++ src/App.tsx
@@ -621,10 +621,10 @@ const Biography = () => (
             <div className="absolute inset-0 bg-red-600 blur-2xl opacity-20 rounded-full"></div>
             <div className="w-32 h-32 md:w-44 md:h-44 rounded-full border-2 border-red-600 p-1 relative z-10">
               <img 
-              src="/perfil.png" // <--- Mude para o nome exato do seu arquivo (ex: natan.jpg)
-              alt="Natan Campos"
-              classnName="w-full h-full rounded-full object-cover grayscale-[30%] hover:grayscale-0 transition-all duration-500"
-              />
+  src="/perfil.png" 
+  alt="Natan Campos"
+  className="w-full h-full rounded-full object-cover grayscale-[30%] hover:grayscale-0 transition-all duration-500"
+/>
             </div>
           </div>
         </div>
